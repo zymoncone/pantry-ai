79c83e4cca94e638e3dac47022481876220d4c80
Szymon <szymon@sarnowicz.net>
2023-10-01 23:01:59 -0500

    update list and removing of items

--- pantry-ai/src/App.js
+++ pantry-ai/src/App.js
@@ -1,14 +1,26 @@
 import { useState, useEffect } from 'react'
+// import { Animated } from "react-animated-css"
 
 function App() {
 
-  const [items, setItems] = useState([{val: "Chicken Breast", quant:"2 lbs"}, {val: "Bananas", quant:"x6"}, {val: "Apples", quant:"x4"}])
+  const sample_list = [{val:"Chicken Breast", quant:"2lbs"}, 
+                       {val:"Bananas", quant:"x6"},
+                       {val:"Pinto Beans", quant:"10oz"},
+                       {val:"Cucumber", quant:"x1"},
+                       {val:"Grape Tomatoes", quant:"x4"},
+                       {val:"Ritz Crackers", quant:"1box"},
+                       {val:"Sliced Ham", quant:"x8"},
+                       {val:"Plain Greek Yogurt", quant:"8oz"},
+                       {val:"Apples", quant:"x4"}]
+
+  const [items, setItems] = useState(sample_list)
   const [openItems, setOpenItems] = useState(false)
   const [itemName, setItemName] = useState("")
   const [quant, setQuant] = useState("")
+  const [unit, setUnit] = useState("amount")
+  const [displayQuant, setDisplayQuant] = useState("")
   
   const handleAddItems = () => {
-    console.log(openItems)
     if (openItems === false) {
       setOpenItems(true)
     } else {
@@ -16,11 +28,28 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    if (unit === "amount") {
+      setDisplayQuant("x" + quant)
+    } else {
+      setDisplayQuant(quant + unit)
+    }
+  }, [unit, quant])
+
   const handleNewItem = () => {
-    let newEntry = {val: itemName, quant: quant}
-    setItems(items => [...items, newEntry])
+    let newEntry = {val: itemName, quant: displayQuant}
+    if (quant !== "" && itemName !== "") {
+      setItems(items => [...items, newEntry])
+      setQuant("")
+      setItemName("")
+    }
   }
 
+  const handleDeleteItem = (e) => {
+    setItems(items => items.filter((_, index, __) => {return index.toString() !== e.target.value.toString()}))
+  }
+
+  console.log(unit)
   return (
     <div className="app">
       <section className="main">
@@ -31,15 +60,30 @@ function App() {
         <button>Generate Recipes</button>
       </div>
       {openItems && <div className='item-list'>
+        <input placeholder='Item Name' value={itemName} onChange={(e) => setItemName(e.target.value)} autoComplete="off"></input>
         <div className='form'>
-          <input placeholder='Item Name' value={itemName} onChange={(e) => setItemName(e.target.value)} autoComplete="off"></input>
-          <input placeholder='Quantity' value={quant} onChange={(e) => setQuant(e.target.value)} autoComplete="off"></input>
+          <input className="quantity" placeholder='Quantity' value={quant} onChange={(e) => setQuant(e.target.value)} autoComplete="off"></input>
+          <select name='unit' onChange={(e) => setUnit(e.target.value)}>
+              <option value='amount'>amount</option>
+              <option value='lbs'>lbs</option>
+              <option value='oz'>oz</option>
+              <option value='g'>g</option>
+              <option value='liters'>liters</option>
+              <option value='gals'>gals</option>
+              <option value='box'>box</option>
+            </select>
           <button onClick={handleNewItem}>Submit</button>
         </div>
         {items.map((item, index) => (
-          <div key={index} className='items'>
-            {item.val} {item.quant}
-          </div>
+          <ul key={index}>
+            <li className='items'>
+              {item.val}
+            </li>
+            <li className='quant'>
+              {item.quant}
+            </li>
+            <button className='delete-button' value={index} onClick={handleDeleteItem}>X</button>
+          </ul>
         ))}
       </div>}
       </section>
